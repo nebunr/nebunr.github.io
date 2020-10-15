a40f3562bb16f09422212000217fc10820c382ad
Benjamin Estela <benjaminestela@nevada.unr.edu>
2020-10-15 14:26:36 -0700

    Added jQuery functionality
    Moved many files around too

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# [nebunr.github.io](https://nebunr.github.io/index.html)
+# Website: [nebunr.github.io](https://nebunr.github.io/)
 
-I am [Benjamin Estela](https://github.com/nebunr), a recent computer science graduate from the University of Nevada, Reno. The above GitHub Page is intended to host my projects, resume, and more.
+I am [Benjamin Estela](https://github.com/nebunr), a recent computer science graduate from the [University of Nevada, Reno](https://www.unr.edu/cse). The above GitHub Page is intended to host my projects, resume, and more.
---
+++ index.js
@@ -0,0 +1,44 @@
+//JavaScript for index
+
+// Modal Image Gallery
+function onClick(element) {
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+    var captionText = document.getElementById("caption");
+    captionText.innerHTML = element.alt;
+}
+
+// Change style of navbar on scroll
+window.onscroll = function() {scrollFunc()};
+function scrollFunc() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white" + " w3-text-dark-grey";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
+    }
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleFunc() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+//jQuery Functions
+$(document).ready(function() {
+    //Smooth scrolling when using clicking to a #id
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if(target.length) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 800);
+        }
+    });
+});
